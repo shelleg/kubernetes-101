721fed5434d7d844ca7844796f8bcb95154cb691
Rafael Bodill <justrafi@gmail.com>
2018-05-28 17:21:27 +0300

    Add app client and server

---
+++ app/client/client.js
@@ -0,0 +1,43 @@
+var http = require('http');
+const express = require('express')
+const app = express()
+const PING_PORT = process.env.PING_PORT || 8081;
+const PONG_HOST = process.env.PONG_HOST || 'app.exmaple.com';
+const PONG_PORT = process.env.PONG_PORT || 8080;
+
+// API Endpoints
+app.get('/', function (req, res) {
+  res.send('Hello World!')
+})
+
+app.get('/isAlive', function (req, res) {
+  res.send('It\'s aaaalive!')
+})
+
+// Metrics endpoint
+app.get('/ping', (req, res) => {
+  var options = {
+    host: PONG_HOST,
+    method: 'GET',
+    path: '/pong',
+    port: PONG_PORT
+  }
+
+  console.log(req.headers);
+
+  var request = http.request(options, (response) => {
+    res.send('pong\n');
+  });
+
+  request.end();
+});
+
+process.on('SIGINT', function() {
+    console.log("Caught interrupt signal");
+    process.exit();
+});
+
+// Start app
+app.listen(PING_PORT, function () {
+  console.log(`App listening on port ${PING_PORT}...`)
+})
